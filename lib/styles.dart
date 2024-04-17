dd100dc59fc91496697b5599edbe57cd3bb5286c
Sami W <samiwang2k@gmail.com>
2024-04-17 09:17:07 -0400

    namerings

--- lib/styles.dart
+++ lib/styles.dart
@@ -5,24 +5,27 @@ const colorGray = Color.fromRGBO(141, 141, 141, 1.0);
 const colorWhite = Colors.white;
 
 const TextTheme textThemeDefault = TextTheme(
-  headline1:
+  displayLarge:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 26),
-  headline2:
+  displayMedium:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 22),
-  headline3:
+  displaySmall:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 20),
-  headline4:
+  headlineMedium:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 16),
-  headline5:
+  headlineSmall:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 14),
-  headline6:
+  titleLarge:
       TextStyle(color: colorBlack, fontWeight: FontWeight.w700, fontSize: 12),
-  bodyText1:
-      TextStyle(color: colorBlack, fontSize: 14, fontWeight: FontWeight.w500, height: 1.5),
-  bodyText2:
-      TextStyle(color: colorGray, fontSize: 14, fontWeight: FontWeight.w500, height: 1.5),
-  subtitle1:
-    TextStyle(color: colorBlack, fontSize: 12, fontWeight: FontWeight.w400),
-  subtitle2:
-    TextStyle(color: colorGray, fontSize: 12, fontWeight: FontWeight.w400),
+  bodyLarge: TextStyle(
+      color: colorBlack,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      height: 1.5),
+  bodyMedium: TextStyle(
+      color: colorGray, fontSize: 14, fontWeight: FontWeight.w500, height: 1.5),
+  titleMedium:
+      TextStyle(color: colorBlack, fontSize: 12, fontWeight: FontWeight.w400),
+  titleSmall:
+      TextStyle(color: colorGray, fontSize: 12, fontWeight: FontWeight.w400),
 );
